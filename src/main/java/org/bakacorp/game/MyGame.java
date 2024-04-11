16ffcaeb036bcd99d3f59c5ab2f0102891fc1967
Mental Onigiri <onigirimental@skiff.com>
2024-04-11 15:45:38 +1000

    move java to src/main/java

---
+++ src/main/java/org/bakacorp/game/MyGame.java
@@ -0,0 +1,26 @@
+package org.bakacorp.game;
+
+import org.libsdl.app.SDLActivity;
+
+/**
+ * A sample wrapper class that just calls SDLActivity
+ */
+
+public class MyGame extends SDLActivity {
+    /**
+     * This method is called by SDL before loading the native shared libraries.
+     * It can be overridden to provide names of shared libraries to be loaded.
+     * The default implementation returns the defaults. It never returns null.
+     * An array returned by a new implementation must at least contain "SDL2".
+     * Also keep in mind that the order the libraries are loaded may matter.
+     *
+     * @return names of shared libraries to be loaded (e.g. "SDL2", "main").
+     */
+    @Override
+    protected String[] getLibraries() {
+        return new String[]{
+                "SDL2",
+                "jni-main"
+        };
+    }
+}
